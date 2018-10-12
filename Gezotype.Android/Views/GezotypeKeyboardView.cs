40d5226d66c11a7e8ac07afd70c93b64144667ec
mute--- <vadim-rom@yandex.ru>
2018-10-13 01:38:13 +0300

    Add touch paint

--- Gezotype.Android/Views/GezotypeKeyboardView.cs
+++ Gezotype.Android/Views/GezotypeKeyboardView.cs
@@ -4,11 +4,17 @@ using Android.InputMethodServices;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
+using System;
+using Gezotype.PCL;
 
 namespace Gezotype.Android.Views
 {
     public class GezotypeKeyboardView : View
     {
+        public event EventHandler<CharRecognizedEventArgs> CharRecongnized;
+
+        private readonly Path _touchPath = new Path();
+
         public GezotypeKeyboardView(Context context, IAttributeSet set) : base(context, set) { }
 
         protected override void OnDraw(Canvas canvas)
@@ -16,18 +22,48 @@ namespace Gezotype.Android.Views
             var cx = MeasuredWidth / 2;
             var cy = MeasuredHeight / 2;
 
-            var paint = new Paint();
-            paint.SetStyle(Paint.Style.Fill);
-            paint.Color = Color.Green;
+            var paint = new Paint
+            {
+                Color = Color.Gray
+            };
+            paint.SetStyle(Paint.Style.Stroke);
 
-            canvas.DrawRect(cx - 50, cy - 50, cx + 50, cy + 50, paint);
-
-            base.OnDraw(canvas);
+            canvas.DrawPath(_touchPath, paint);
         }
 
         protected override void OnMeasure(int widthMeasureSpec, int heightMeasureSpec)
         {
             SetMeasuredDimension(widthMeasureSpec, widthMeasureSpec);
+        }
+
+        public override bool OnTouchEvent(MotionEvent e)
+        {
+            switch (e.Action)
+            {
+                case MotionEventActions.Down:
+                    _touchPath.Reset();
+                    _touchPath.MoveTo(e.GetX(), e.GetY());
+                    Invalidate();
+                    return true;
+
+                case MotionEventActions.Move:
+                    _touchPath.LineTo(e.GetX(), e.GetY());
+                    Invalidate();
+                    return true;
+
+                case MotionEventActions.Up:
+                    _touchPath.Reset();
+                    Invalidate();
+                    return true;
+
+                default:
+                    return base.OnTouchEvent(e);
+            }
+        }
+
+        private void RaiseCharRecongnized(string @char)
+        {
+            CharRecongnized?.Invoke(this, new CharRecognizedEventArgs(@char));
         }
     }
 }
