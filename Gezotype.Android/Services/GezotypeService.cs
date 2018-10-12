9b492d3173cf8659ccea5c2151160ec693c38fb4
mute--- <vadim-rom@yandex.ru>
2018-10-13 00:33:41 +0300

    proto

---
+++ Gezotype.Android/Services/GezotypeService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Android;
+using Android.App;
+using Android.InputMethodServices;
+using Android.Views;
+using Gezotype.Android.Views;
+
+namespace Gezotype.Android.Services
+{
+    [Service(Permission = "android.permission.BIND_INPUT_METHOD")]
+    [IntentFilter(new string[] { "android.view.InputMethod" })]
+    [MetaData("android.view.im", Resource = "@xml/method")]
+    public class GezotypeService : InputMethodService
+    {
+        public override View OnCreateInputView()
+        {
+            var input = CurrentInputConnection.CommitText(new Java.Lang.String("Hello from custom keyboard!!!"), 1);
+            var keyboardView = LayoutInflater.Inflate(Resource.Layout.keyboard_view, null);
+            return keyboardView;
+        }
+    }
+}
---
+++ Gezotype.Android/Views/GezotypeKeyboardView.cs
@@ -0,0 +1,33 @@
+﻿using Android.Graphics;
+using Android.Content;
+using Android.InputMethodServices;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace Gezotype.Android.Views
+{
+    public class GezotypeKeyboardView : View
+    {
+        public GezotypeKeyboardView(Context context, IAttributeSet set) : base(context, set) { }
+
+        protected override void OnDraw(Canvas canvas)
+        {
+            var cx = MeasuredWidth / 2;
+            var cy = MeasuredHeight / 2;
+
+            var paint = new Paint();
+            paint.SetStyle(Paint.Style.Fill);
+            paint.Color = Color.Green;
+
+            canvas.DrawRect(cx - 50, cy - 50, cx + 50, cy + 50, paint);
+
+            base.OnDraw(canvas);
+        }
+
+        protected override void OnMeasure(int widthMeasureSpec, int heightMeasureSpec)
+        {
+            SetMeasuredDimension(widthMeasureSpec, widthMeasureSpec);
+        }
+    }
+}
